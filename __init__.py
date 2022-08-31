77acd1747fd802b87db985e63b614c60416acc76
Patric-k-k <104700188+Patric-k-k@users.noreply.github.com>
2022-08-31 13:40:04 -0500

    Initilized project

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from distutils.log import debug
+
+
+def Error(Type, Message, Code, Debug):
+    if Debug == True:
+        print("Remember to set the debug variable to False when you're done debugging! Also, check out the documentation for more information.")
+        print("Type: " + Type)
+        print("Message: " + Message)
+        print("Code: " + Code)
+    try:
+        if Code == 0:
+            print("Error: " + Message)
+        elif Code > 0:
+            print("Error Code: " + str(Code) + "" + str(Type) + " " + str(Message))
+    except:
+        if debug == True:
+            print("An error occurred while trying to print the error message. Please check the documentation at https://patric-k-k.github.io/DOC/Error_handle.html for more information.")
+        elif debug == False:
+            print("Error: " + "Faled to print error message. Please contact the developer(s).")
---
+++ example.py
@@ -0,0 +1,4 @@
+import __init__ as Error
+import math
+print("Random error message being printed")
+number = math.random(1, 15)
